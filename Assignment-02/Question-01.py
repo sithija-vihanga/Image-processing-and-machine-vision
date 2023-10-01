48acd6ad8b6ee610fc641082812fa3067559ff24
Sithija Vihanga Ranaraja <sithija.vihanga28@gmail.com>
2023-10-01 13:42:55 +0530

    Update Question-01.py

--- Assignment-02/Question-01.py
+++ Assignment-02/Question-01.py
@@ -19,7 +19,10 @@ for sigma in sigma_values:
     log = cv2.GaussianBlur(gray, (3, 3), sigmaX=sigma)
     log = cv2.Laplacian(log, cv2.CV_64F)
 
-    #cv2.imshow('blah',log)
+    output_filename = f'log_{sigma}.jpg'
+    cv2.imwrite(output_filename, log)
+    print(f'Saved {output_filename}')
+
     # Find local maxima as extrema in the scale space
     min_dist = int(sigma)  # Minimum distance between circles
     _, thresh_log = cv2.threshold(np.abs(log), 20, 80, cv2.THRESH_BINARY)
@@ -30,7 +33,7 @@ for sigma in sigma_values:
     for contour in contours:
         if len(contour) >= 10:
             approx = cv2.approxPolyDP(contour, 0.3, closed=True)
-            if ((len(approx) > 35) and (len(approx) < 60)):  # Circles are approximated to have 8 points
+            if 35 < len(approx) < 60:  # filter out contours with no. of points to reduce noise
                 (x, y), radius = cv2.minEnclosingCircle(contour)
                 circles.append((int(x), int(y), int(radius)))
 
@@ -38,11 +41,20 @@ for sigma in sigma_values:
     all_circles.extend(circles)
 
     # Draw circles on the image
-    im2 = im.copy()
+    im_with_circles = im.copy()
     for circle in all_circles:
-        cv2.circle(im2, (circle[0], circle[1]), circle[2], (0, 255, 0), 2)  # Draw circles in green
+        cv2.circle(im_with_circles, (circle[0], circle[1]), circle[2], (0, 255, 0), 2)  # Draw circles in green
 
-    # Display the image with circles drawn on it
-    cv2.imshow('Circles Detected', im2)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    # Save the image with circles drawn on it
+    output_filename = f'circles_detected_sigma_{sigma}.jpg'
+    cv2.imwrite(output_filename, im_with_circles)
+    print(f'Saved {output_filename}')
+
+# Find and print details of the largest circle
+largest_circle = max(all_circles, key=lambda x: x[2])
+print(f'Largest circle: Center coordinates: ({largest_circle[0]}, {largest_circle[1]}), Radius: {largest_circle[2]}')
+
+# Display the image with circles drawn on it
+cv2.imshow('Circles Detected', im_with_circles)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
